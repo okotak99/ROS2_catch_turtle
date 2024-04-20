284e5a609bee072835f12c16520792cecdcb51d7
okotak99 <dydxkr0410@gmail.com>
2024-04-20 21:46:37 +0900

    upload new files

---
+++ src/catch_turtle/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os 
+import glob
+
+package_name = 'my_first_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob.glob(os.path.join('launch', '*.launch.py')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='addinedu',
+    maintainer_email='dydxkr0410@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'turtle_controller = my_first_package.turtle_controller:main',
+            'turtle_spawner = my_first_package.turtle_spawner:main'
+        ],
+    },
+)
